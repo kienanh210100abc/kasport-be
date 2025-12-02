78fa0dc655ab1df5192cca84afdeceae96771d27
kienanh210100abc <duckien210100@gmail.com>
2025-12-02 11:32:19 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,18 @@ app.use(express.json()); // Parse JSON body
 
 // Tạo connection pool với MySQL
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
+  host: process.env.DB_HOST, // mysql.railway.internal
+  user: process.env.DB_USER, // root
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
